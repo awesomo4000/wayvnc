ab4d7c22faee2b3708b91d61667a7e92e79b0719
Andri Yngvason <andri@yngvason.is>
2026-06-30 06:48:01 +0000

    json-ipc: Bump jvprintf's buffer to 256

--- src/json-ipc.c
+++ src/json-ipc.c
@@ -208,7 +208,7 @@ json_t* jprintf(const char* fmt, ...)
 
 json_t* jvprintf(const char* fmt, va_list ap)
 {
-	char buffer[128];
+	char buffer[256];
 	int len = vsnprintf(buffer, sizeof(buffer), fmt, ap);
 	return json_stringn(buffer, MIN(sizeof(buffer) - 1, len));
 }
