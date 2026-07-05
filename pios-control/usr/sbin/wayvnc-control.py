18e622021c130ea4479763069a6d1d8e8bf4d7ba
Andri Yngvason <andri@yngvason.is>
2026-07-05 10:11:03 +0000

    pios-control: wayvnc-control: Remove message queues

--- pios-control/usr/sbin/wayvnc-control.py
+++ pios-control/usr/sbin/wayvnc-control.py
@@ -12,10 +12,8 @@ class Program:
 	reader = None
 	writer = None
 	read_buffer = ""
-	message_queue = asyncio.Queue()
-	reply_queue = asyncio.Queue()
 	decoder = json.JSONDecoder()
-	tasks = []
+	replies = {}
 
 	async def read_message(self):
 		while True:
@@ -28,21 +26,28 @@ class Program:
 				self.read_buffer += data.decode('utf-8')
 
 	async def send_command(self, method, params = None):
+		command_id = self.command_seq
+		self.command_seq += 1
+
 		cmd = {
 			"method": method,
-			"id": self.command_seq,
+			"id": command_id,
 		}
 
 		if not params is None:
 			cmd['params'] = params
 
-		self.command_seq += 1
 		self.writer.write(json.dumps(cmd).encode())
 		await self.writer.drain()
 
-		reply = await self.reply_queue.get()
-		self.reply_queue.task_done()
-		return reply['code'] == 0
+		while command_id not in self.replies:
+			message = await self.read_message()
+			if 'code' in message and 'id' in message:
+				self.replies[message['id']] = message
+			else:
+				await self.process_message(message)
+
+		return self.replies.pop(command_id)['code'] == 0
 
 	async def attach(self, display):
 		# TODO: It would be better to pass the socket on to wayvnc as a file descriptor
@@ -69,23 +74,14 @@ class Program:
 		if (method == 'detached'):
 			await self.attach_any_with_retry()
 
-	async def message_processor(self):
-		while True:
-			message = await self.read_message()
-			if 'method' in message:
-				await self.message_queue.put(message)
-			elif 'code' in message:
-				await self.reply_queue.put(message)
-
 	async def main(self):
 		self.reader, self.writer = await asyncio.open_unix_connection("/tmp/wayvnc/wayvncctl.sock")
-		self.tasks.append(asyncio.create_task(self.message_processor()))
 
 		await self.attach_any_with_retry()
 		await self.send_command("event-receive")
 
 		while True:
-			message = await self.message_queue.get()
+			message = await self.read_message()
 			await self.process_message(message)
 
 prog = Program()
